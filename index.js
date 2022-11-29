5fd8f11b3231914aaaca8797cc1b4627164d4d2e
SyedaAishaAhmad <18aishaahmad@gmail.com>
2022-11-30 02:40:29 +0600

    seller account

--- index.js
+++ index.js
@@ -131,6 +131,14 @@ async function run() {
             res.send({ isAdmin: user?.account === 'admin' });
         })
 
+        // Seller
+        app.get('/users/seller/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email }
+            const user = await usersCollection.findOne(query);
+            res.send({ isseller: user?.account === 'seller' });
+        })
+
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
